bc0d09ae352fe3b102e63065e750ef3409a6e263
Shaid Ibna Sobhan (Meharaj) <shaidmeheraj9@gmail.com>
2024-11-24 19:27:40 +0600

    Create A. Shohag Loves Mod.cpp

---
+++ Codeforce/CodeTON Round 9 (Div. 1 + Div. 2)/A. Shohag Loves Mod.cpp
@@ -0,0 +1,36 @@
+/* Bismillahir Rahmanir Raheem */
+/* Shaid Meheraj */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+const int N = 1e5+5;
+void solve(void)
+{
+    int n;
+    cin >> n;
+    //vector<int>v(n);
+    for(int i=1; i<=n*2; i+=2)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+//Alhamdulillah...
+
+
+
